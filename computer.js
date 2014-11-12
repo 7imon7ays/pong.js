3a44ef9532aab91582f15e26e7ed46496effdb89
Simon Chaffetz <simis1000@gmail.com>
2014-11-11 20:49:25 -0800

    Computer is frozen when hit

--- computer.js
+++ computer.js
@@ -1,12 +1,15 @@
 function Computer () {
+  this.isFrozen = false;
   this.paddle = new Paddle(175, 10, 50, 10);
-};
+}
 
 Computer.prototype.render = function () {
   this.paddle.render();
 };
 
 Computer.prototype.update = function (ball) {
+  if (this.isFrozen) return;
+
   var x_pos = ball.x;
   var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
   if (diff < 0 && diff < -4) {
@@ -20,4 +23,18 @@ Computer.prototype.update = function (ball) {
   } else if (this.paddle.x + this.paddle.width > 400) {
     this.paddle.x = 400 - this.paddle.width;
   }
+
+  if (!Rocket.instances.human) return;
+
+  var humanRocket = Rocket.instances.human,
+      isVertiHit = humanRocket.x > this.paddle.x && humanRocket.x < this.paddle.width + this.paddle.x,
+      isHoriHit = humanRocket.y < this.paddle.y;
+
+  if (isVertiHit && isHoriHit) {
+    this.isFrozen = true;
+    setTimeout(function () {
+      this.isFrozen = false;
+    }.bind(this), 2000);
+  }
 };
+
